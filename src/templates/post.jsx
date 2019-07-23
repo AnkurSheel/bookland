37b646c3ea58ae8d34c7d8834e7ca64e4d13c6cc
Ankur Sheel <ankur.sheel@gmail.com>
2019-07-23 16:31:54 +1200

    show post data

---
+++ src/templates/post.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Layout from '../components/layouts';
+import ReadLink from '../components/readLink';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { graphql } from 'gatsby';
+
+export const query = graphql`
+    query($slug: String!) {
+        mdx(frontmatter: { slug: { eq: $slug } }) {
+            frontmatter {
+                title
+                author
+            }
+            body
+        }
+    }
+`;
+
+const PostTemplate = ({ data: { mdx: post } }) => (
+    <Layout>
+        <h1>{post.frontmatter.title}</h1>
+        <p> Posted by {post.frontmatter.author}</p>
+        <MDXRenderer>{post.body}</MDXRenderer>
+        <ReadLink to="/">&larr; Back to Home</ReadLink>
+    </Layout>
+);
+
+export default PostTemplate;
