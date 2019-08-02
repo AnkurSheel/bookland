244c6e83f80f047ae706243825399bde399878f4
Ankur Sheel <ankur.sheel@gmail.com>
2019-08-02 15:33:07 +1200

    remove some global styles

--- src/components/globalStyles.tsx
+++ src/components/globalStyles.tsx
@@ -1,5 +1,5 @@
+import { css, Global } from '@emotion/core';
 import React from 'react';
-import { Global, css } from '@emotion/core';
 
 const GlobalStyles = () => (
     <Global
@@ -11,12 +11,6 @@ const GlobalStyles = () => (
                 color: '#555',
                 margin: 0,
                 padding: 0,
-
-                // display and flexDirection is used to not show the extra vertical scroll bar when minHeight is 100vh.
-                // https://stackoverflow.com/questions/42508504/min-height-100vh-creates-vertical-scrollbar-even-though-content-is-smaller-than
-                minHeight: '100vh',
-                display: 'flex',
-                flexDirection: 'column',
                 fontFamily: '-apple-system, BlinkMacSystemFont, "Segoue UI", Roboto, Arial, sans-serif',
                 lineHeight: 1.5,
                 fontsize: 16,
