104195f3703248acaad5c0d31eb355d76c1982c0
Ankur Sheel <ankur.sheel@gmail.com>
2020-03-09 07:00:57 +1300

    wip

---
+++ src/@codinators/gatsby-theme-blog/tokens/theme.ts
@@ -0,0 +1,62 @@
+export default {
+    colors: {
+        background: '#463366',
+        backgroundArticle: '#301e4e',
+        paragraph: '#c9bae2',
+        title: '#FFFFFF',
+        subTitle: '#FFFFFF',
+        link: '#ff6e6c',
+        card: {
+            background: '#e2daeb',
+            title: '#1f1135',
+            paragraph: '#1b1325',
+            link: '#1b1325',
+        },
+        tag: {
+            background: '#FFFFFF',
+            link: '#1f1235',
+        },
+        header: {
+            background: '#301e4e',
+            text: '#ff6e6c',
+            border: '#FFFFFF',
+        },
+        footer: {
+            background: '#301e4e',
+            paragraph: '#f4f0fc',
+            link: '#ff6e6c',
+        },
+        notFound: {
+            kawaiiColor: 'red',
+        },
+        codeHighlight: {
+            background: 'red',
+            border: 'red',
+        },
+        imageGallery: {
+            imageBorder: 'red',
+            button: {
+                border: 'red',
+                background: 'red',
+                text: 'red',
+            },
+        },
+        modal: {
+            background: 'red',
+            border: 'red',
+            text: 'red',
+            button: {
+                background: 'red',
+                border: 'red',
+                color: 'red',
+            },
+        },
+
+        overlay: 'rgba(0,0,0,0.5)',
+    },
+    media: {
+        small: '(min-width: 480px)',
+        medium: '(min-width: 700px)',
+        large: '(min-width: 960px)',
+    },
+};
