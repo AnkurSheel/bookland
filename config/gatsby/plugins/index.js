cecaac2488fb71e2c1cf7f6149ecf7c8d9ca9ec6
Ankur Sheel <ankur.sheel@gmail.com>
2021-05-03 14:54:30 +1200

    remove js files

--- config/gatsby/plugins/index.js
+++
@@ -1,7 +0,0 @@
-const analyticOptions = require('./analyticOptions');
-const manifestOptions = require('./manifestOptions');
-
-module.exports = {
-    analytic: analyticOptions,
-    manifest: manifestOptions,
-};
--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    '*.+(js|json|yml|yaml|css|less|scss|ts|tsx|md|mdx|graphql)': ['prettier --write'],
-    '*.+(md|mdx)': ['markdownlint *.mdx'],
-    '*.+(js|jsx|ts|tsx)': ['eslint --fix'],
-};
