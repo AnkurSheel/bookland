e839a19d7bbf6a595aa053dbb8aaceb2f88e3e68
Ankur Sheel <ankur.sheel@gmail.com>
2021-08-20 22:11:04 +1200

    add PostPipeline

---
+++ Bookland/src/Pipelines/PostPipeline.cs
@@ -0,0 +1,44 @@
+﻿using Bookland.Extensions;
+using Statiq.Common;
+using Statiq.Core;
+using Statiq.Markdown;
+using Statiq.Razor;
+using Statiq.Yaml;
+
+namespace Bookland.Pipelines
+{
+    public class PostPipeline : Pipeline
+    {
+        public PostPipeline()
+        {
+            InputModules = new ModuleList
+            {
+                new ReadFiles("posts/**/*.md")
+            };
+
+            ProcessModules = new ModuleList
+            {
+                new ExtractFrontMatter(new ParseYaml()),
+                new RenderMarkdown().UseExtensions(),
+                new OptimizeFileName(),
+                new SetDestination(
+                    Config.FromDocument(
+                        (doc, ctx) =>
+                        {
+                            var postDetailsFromPath = doc.GetPostDetailsFromPath();
+                            return new NormalizedPath("blog").Combine($"{postDetailsFromPath["slug"]}.html");
+                        })),
+            };
+
+            PostProcessModules = new ModuleList
+            {
+                new RenderRazor().WithBaseModel()
+            };
+
+            OutputModules = new ModuleList
+            {
+                new WriteFiles()
+            };
+        }
+    }
+}
