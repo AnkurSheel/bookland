8c77c5116522898e474c516a5ad15e38682d35aa
Ankur Sheel <ankur.sheel@gmail.com>
2021-08-20 22:13:39 +1200

    Add assets pipeline

---
+++ Bookland/src/Pipelines/AssetsPipeline.cs
@@ -0,0 +1,17 @@
+﻿using Bookland.Extensions;
+using Statiq.Common;
+using Statiq.Core;
+
+namespace Bookland.Pipelines
+{
+    public class AssetsPipeline : Pipeline
+    {
+        public AssetsPipeline()
+        {
+            InputModules = new ModuleList
+            {
+                new CopyFiles("assets/**/*.{*,!scss,!css}"),
+            };
+        }
+    }
+}
