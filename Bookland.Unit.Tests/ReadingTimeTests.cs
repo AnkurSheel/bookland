513369608405ae727f5ecdde45546f9f4eb3f344
Ankur Sheel <ankur.sheel@gmail.com>
2021-09-11 16:00:18 +1200

    add tests

---
+++ Bookland.Unit.Tests/ReadingTimeTests.cs
@@ -0,0 +1,48 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Bookland.Modules;
+using Statiq.Common;
+using Statiq.Testing;
+using Xunit;
+
+namespace Bookland.Unit.Tests
+{
+    public class ReadingTimeTests : BaseFixture
+    {
+        [Theory]
+        [InlineData(100, 0, 30)]
+        [InlineData(200, 1, 0)]
+        [InlineData(300, 1, 30)]
+        [InlineData(400, 2, 0)]
+        public async Task If_WPM_is_not_overridden_readingTime_MetaData_is_added_using_200WPM(int numberOfWords, int expectedMinutes, int expectedSeconds)
+        {
+            string input = string.Concat(Enumerable.Repeat("a ", numberOfWords));
+
+            TestDocument document = new TestDocument(input);
+            ReadingTime readingTime = new ReadingTime();
+
+            TestDocument result = await ExecuteAsync(document, readingTime).SingleAsync();
+
+            var readingTimeData = result.Get<ReadingTimeData>(MetaDataKeys.ReadingTime);
+
+            Assert.Equal(new ReadingTimeData(expectedMinutes, expectedSeconds, numberOfWords), readingTimeData);
+        }
+
+        [Theory]
+        [InlineData(400, 200, 0, 30)]
+        [InlineData(300, 300, 1, 0)]
+        public async Task ReadingTime_MetaData_is_added_correctly_when_WPM_is_overriden(int wordsPerMinute, int numberOfWords, int expectedMinutes, int expectedSeconds)
+        {
+            string input = string.Concat(Enumerable.Repeat("a ", numberOfWords));
+
+            TestDocument document = new TestDocument(input);
+            ReadingTime readingTime = new ReadingTime(wordsPerMinute);
+
+            TestDocument result = await ExecuteAsync(document, readingTime).SingleAsync();
+
+            var readingTimeData = result.Get<ReadingTimeData>(MetaDataKeys.ReadingTime);
+
+            Assert.Equal(new ReadingTimeData(expectedMinutes, expectedSeconds, numberOfWords), readingTimeData);
+        }
+    }
+}
