a3f44a48d9a89285fc71f092a369c0904eddd751
Ankur Sheel <ankur.sheel@gmail.com>
2021-09-18 13:22:06 +1200

    add tailwind forms

--- node/tailwind.config.js
+++ node/tailwind.config.js
@@ -52,5 +52,5 @@ module.exports = {
     variants: {
         extend: {},
     },
-    plugins: [require('@tailwindcss/typography')],
+    plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 };
