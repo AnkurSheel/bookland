18f2fe8364543d5fee56661f6b71e9fc73f81c7e
Ankur Sheel <ankur.sheel@gmail.com>
2021-09-19 02:13:40 +1200

    resize only checked out  jpegs

--- src/Bookland/src/Commands/ResizeJpeg.cs
+++ src/Bookland/src/Commands/ResizeJpeg.cs
@@ -1,7 +1,10 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using ByteSizeLib;
+using LibGit2Sharp;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SixLabors.ImageSharp;
@@ -9,6 +12,8 @@ using SixLabors.ImageSharp.Processing;
 using Spectre.Console.Cli;
 using Statiq.App;
 using Statiq.Common;
+using Statiq.Core;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace Bookland.Commands
 {
@@ -21,6 +26,10 @@ namespace Bookland.Commands
         [CommandArgument(0, "<height>")]
         [Description("Specify the target height")]
         public int Height { get; set; }
+
+        [CommandOption("-c")]
+        [Description("Compress all files, not just uncommitted files")]
+        public bool AllFiles { get; set; }
     }
 
     [Description(
@@ -37,15 +46,47 @@ namespace Bookland.Commands
 
         protected override async Task<int> ExecuteEngineAsync(CommandContext commandContext, ResizeJpegSettings commandSettings, IEngineManager engineManager)
         {
-            var jpegs = engineManager.Engine.FileSystem.GetInputFiles("**/*.{jpg, jpeg}").Select(x => x.Path).ToList();
+            Engine engine = engineManager.Engine;
+            var jpegs = GetImages(!commandSettings.AllFiles, engine.FileSystem);
+
+            var message = commandSettings.AllFiles
+                ? "all files"
+                : "checked out files";
+            engineManager.Engine.Logger.Log(LogLevel.Information, "Beginning resizing of images on {message} : {count}", message, jpegs.Count);
+
+            await ResizeImages(jpegs, commandSettings.Width, commandSettings.Height, engineManager.Engine);
+
+            return 0;
+        }
+
+        private List<NormalizedPath> GetImages(bool onlyCheckedOutFiles, IFileSystem fileSystem)
+        {
+            var jpegs = fileSystem.GetInputFiles("**/*.{jpg, jpeg}").Select(x => x.Path).ToList();
+
+            if (onlyCheckedOutFiles)
+            {
+                var rootPath = fileSystem.RootPath.Parent.Parent.FullPath;
+                using var repo = new Repository(rootPath);
+                var status = repo.RetrieveStatus();
+
+                var modifiedJpegs = status.Where(x => Path.GetExtension(x.FilePath) == ".jpg" && x.State != FileStatus.Ignored)
+                    .Select(x => new NormalizedPath(Path.Combine(rootPath, x.FilePath)))
+                    .ToList();
+
+                jpegs = jpegs.Where(x => modifiedJpegs.Contains(x)).ToList();
+            }
+
+            return jpegs;
+        }
+
+        private async Task ResizeImages(IReadOnlyList<NormalizedPath> jpegs, int newWidth, int newHeight, IEngine engine)
+        {
             var totalPre = 0l;
             var totalPost = 0l;
 
-            engineManager.Engine.Logger.Log(LogLevel.Information, "Beginning resizing of images");
-
             foreach (var jpeg in jpegs)
             {
-                var preSize = engineManager.Engine.FileSystem.GetFile(jpeg.FullPath).Length;
+                var preSize = engine.FileSystem.GetFile(jpeg.FullPath).Length;
                 totalPre += preSize;
 
                 var image = await Image.LoadAsync(jpeg.FullPath);
@@ -53,21 +94,21 @@ namespace Bookland.Commands
                 var originalSize = image.Size();
                 var resizeOptions = new ResizeOptions
                 {
-                    Size = new Size(commandSettings.Width, commandSettings.Height),
+                    Size = new Size(newWidth, newHeight),
                     Compand = true
                 };
 
                 image.Mutate(imageContext => imageContext.Resize(resizeOptions));
 
                 await image.SaveAsJpegAsync(jpeg.FullPath);
-                var postSize = engineManager.Engine.FileSystem.GetFile(jpeg.FullPath).Length;
+                var postSize = engine.FileSystem.GetFile(jpeg.FullPath).Length;
                 totalPost += postSize;
 
                 var percentChanged = postSize > preSize
                     ? postSize / (decimal)preSize - 1
                     : 1 - postSize / (decimal)preSize;
-                
-                engineManager.Engine.Logger.Log(
+
+                engine.Logger.Log(
                     LogLevel.Information,
                     "Resized {Path} with {Size}. Changed from {PreSize} to {PostSize} ({PercentChanged:P})",
                     jpeg.GetRelativeInputPath(),
@@ -77,13 +118,7 @@ namespace Bookland.Commands
                     percentChanged);
             }
 
-            engineManager.Engine.Logger.Log(
-                LogLevel.Information,
-                "Resizing complete. Updated Size from {PreSize} to {PostSize}",
-                ByteSize.FromBytes(totalPre).ToString(),
-                ByteSize.FromBytes(totalPost).ToString());
-
-            return 0;
+            engine.Logger.Log(LogLevel.Information, "Resizing complete. Updated Size from {PreSize} to {PostSize}", ByteSize.FromBytes(totalPre).ToString(), ByteSize.FromBytes(totalPost).ToString());
         }
     }
 }
